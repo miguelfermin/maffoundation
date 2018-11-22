78bd6b5caee53141f303f4fb768d2679751241e3
Miguel Fermin <miguel.fermin.dev@gmail.com>
2018-11-21 21:07:13 -0500

    added xcode project

---
+++ MAFFoundation/MAFFoundation.h
@@ -0,0 +1,19 @@
+//
+//  MAFFoundation.h
+//  MAFFoundation
+//
+//  Created by Miguel Fermin on 11/21/18.
+//  Copyright © 2018 MAF Software LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MAFFoundation.
+FOUNDATION_EXPORT double MAFFoundationVersionNumber;
+
+//! Project version string for MAFFoundation.
+FOUNDATION_EXPORT const unsigned char MAFFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MAFFoundation/PublicHeader.h>
+
+
